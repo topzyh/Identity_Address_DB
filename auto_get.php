ceb8c5f674e3aee6757e0c7396bdbf4057efbbc0
Topzyh <zyh49634@ly.com>
2018-08-30 00:07:27 +0800

    强迫症的我格式化了一下

--- auto_get.php
+++ auto_get.php
@@ -1,36 +1,40 @@
 <?php
+/**
+ * 自动拉取最新的列表
+ * @author TopZYH <zyh.95@163.com>
+ */
 
 // 获取数据
 $source_data = file_get_contents('https://way.jd.com/RTBAsia/dictionary_district?appkey=9b066f875202e509f662b86efdc990d8');
-$source_data = json_decode($source_data,true);
-if(empty($source_data['result'])){
-	die("未拉取到数据:".$source_data['msg']);
+$source_data = json_decode($source_data, true);
+if (empty($source_data['result'])) {
+	die("未拉取到数据:" . $source_data['msg']);
 }
 
 // 数据格式化
 $source_data = explode("\n", $source_data['result']);
-$data = [];
+$data        = [];
 foreach ($source_data as $value) {
-	$value = explode(',', $value);
-	$data[(int)$value[0]] = $value;
+	$value                 = explode(',', $value);
+	$data[(int) $value[0]] = $value;
 }
 unset($source_data);
 // var_dump($data); // 有效数据
 
 // 连接数据库
-$conn=new mysqli('localhost:3306','root','topsts');//连接数据库
-if($conn==false){
-    die("数据连接失败！") ;
-}else{
-    echo "数据连接成功！<br>\n";
+$conn = new mysqli('localhost:3306', 'root', 'topsts'); //连接数据库
+if ($conn == false) {
+	die("数据连接失败！");
+} else {
+	echo "数据连接成功！<br>\n";
 }
 $conn->query("set names 'utf8'"); //数据库输出编码
 $conn->select_db('test'); //打开数据库
 
 // 先把所有status=0
 $sql = "UPDATE `system_idtoad` SET status=0";
-if($conn->query($sql)!=true){
-	echo "Error insert data: " . $conn->error .'SQL:'.$sql."<br>\n";
+if ($conn->query($sql) != true) {
+	echo "Error insert data: " . $conn->error . 'SQL:' . $sql . "<br>\n";
 }
 
 // 写入数据库
@@ -39,16 +43,16 @@ foreach ($data as $value) {
 	$result = $conn->query("SELECT * FROM `system_idtoad` WHERE id=$value[0]");
 	if ($temp = $result->fetch_assoc()) {
 		$sql = "UPDATE `system_idtoad` SET count_name='$value[1]',city_name='$value[2]',province_name='$value[3]',status=1 WHERE id=$value[0]";
-		if($conn->query($sql)!=true){
-			echo "Error insert data: " . $conn->error .'SQL:'.$sql."<br>\n";
+		if ($conn->query($sql) != true) {
+			echo "Error insert data: " . $conn->error . 'SQL:' . $sql . "<br>\n";
 		}
 	} else {
-		print_r($value);echo"<br>\n"; // 输出新增
+		print_r($value);
+		echo "<br>\n"; // 输出新增
 		$sql = "INSERT INTO system_idtoad (id, province_name, city_name, count_name, status) VALUES ($value[0], '$value[3]', '$value[2]', '$value[1]', 1)";
-		if($conn->query($sql)!=true){
-			echo "Error insert data: " . $conn->error.'SQL:'.$sql."<br>\n";
+		if ($conn->query($sql) != true) {
+			echo "Error insert data: " . $conn->error . 'SQL:' . $sql . "<br>\n";
 		}
 	}
 }
 echo "更新完成！";
-
--- identity.php
+++ identity.php
@@ -3,70 +3,64 @@
  * 身份证号码处理
  * @author TopZYH <zyh.95@163.com>
  */
-class identity
-{
+class identity {
+	/**
+	 * 判断身份证号码是否正确
+	 */
+	public function is_identity($identity) {
+		if (strlen($identity) != 18) {
+			return false;
+		}
+		$identity_body     = substr($identity, 0, 17); // 身份证主体(前17位)
+		$identity_lastCode = strtoupper(substr($identity, 17, 1)); // 身份证最后一位的验证码
+		$factor            = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2); // 加权因子
+		$code              = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'); // 校验码对应值
+		$checksum          = 0;
 
-    /**
-     * 判断身份证号码是否正确
-     */
-    public function is_identity($identity)
-    {
-        if (strlen($identity) != 18) {
-            return false;
-        }
-        $identity_body     = substr($identity, 0, 17); // 身份证主体(前17位)
-        $identity_lastCode = strtoupper(substr($identity, 17, 1)); // 身份证最后一位的验证码
-        $factor            = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2); // 加权因子
-        $code              = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'); // 校验码对应值
-        $checksum          = 0;
+		for ($i = 0; $i < strlen($identity_body); $i++) {
+			$checksum += substr($identity_body, $i, 1) * $factor[$i];
+		}
+		// $code[$checksum % 11] 为计算后的最后一位效验码
+		if ($code[$checksum % 11] != $identity_lastCode) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-        for ($i = 0; $i < strlen($identity_body); $i++) {
-            $checksum += substr($identity_body, $i, 1) * $factor[$i];
-        }
-        // $code[$checksum % 11] 为计算后的最后一位效验码
-        if ($code[$checksum % 11] != $identity_lastCode) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+	/**
+	 * 通过身份证号码获取性别 男1 女0
+	 */
+	public function identity_get_sex($identity) {
+		return $sex = (substr($identity, 16, 1) % 2) ? 1 : 0;
+	}
 
-    /**
-     * 通过身份证号码获取性别 男1 女0
-     */
-    public function identity_get_sex($identity)
-    {
-        return $sex = (substr($identity, 16, 1) % 2) ? 1 : 0;
-    }
+	/**
+	 * 通过身份证号码获取生日
+	 */
+	public function identity_get_birthday($identity) {
+		$year  = ((int) substr($identity, 6, 4)); //取得年份
+		$month = ((int) substr($identity, 10, 2)); //取得月份
+		$day   = ((int) substr($identity, 12, 2)); //取得几号
+		// return $birthday = $year . '-' . $month . '-' . $day; // 返回文本
+		return mktime(0, 0, 0, $month, $day, $year); // 返回时间戳
+	}
 
-    /**
-     * 通过身份证号码获取生日
-     */
-    public function identity_get_birthday($identity)
-    {
-        $year  = ((int) substr($identity, 6, 4)); //取得年份
-        $month = ((int) substr($identity, 10, 2)); //取得月份
-        $day   = ((int) substr($identity, 12, 2)); //取得几号
-        // return $birthday = $year . '-' . $month . '-' . $day; // 返回文本
-        return mktime(0, 0, 0, $month, $day, $year); // 返回时间戳
-    }
-
-    /**
-     * 通过身份证号码获取籍贯，需要调用数据库（配合目录下.sql文件使用）
-     */
-    public function identity_get_native($identity)
-    {
-        $id6    = substr($identity, 0, 6);
-        $result = dbq("SELECT ad FROM `system_idtoad` WHERE id=$id6");
-        $row    = mysql_fetch_assoc($result);
-        if ($row['ad']) {
-            return $row['ad'];
-        } else {
-            // 处理已经过时的区县，只显示到市级
-            $id6n   = substr($id6, 0, 4) . '00';
-            $result = dbq("SELECT ad FROM `system_idtoad` WHERE id=$id6n");
-            $row    = mysql_fetch_assoc($result);
-            return $row['ad'];
-        }
-    }
+	/**
+	 * 通过身份证号码获取籍贯，需要调用数据库（配合目录下.sql文件使用）
+	 */
+	public function identity_get_native($identity) {
+		$id6    = substr($identity, 0, 6);
+		$result = dbq("SELECT ad FROM `system_idtoad` WHERE id=$id6");
+		$row    = mysql_fetch_assoc($result);
+		if ($row['ad']) {
+			return $row['ad'];
+		} else {
+			// 处理已经过时的区县，只显示到市级
+			$id6n   = substr($id6, 0, 4) . '00';
+			$result = dbq("SELECT ad FROM `system_idtoad` WHERE id=$id6n");
+			$row    = mysql_fetch_assoc($result);
+			return $row['ad'];
+		}
+	}
 }
