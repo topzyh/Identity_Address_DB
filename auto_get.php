733084e0ef0cf5f14c0b58c0345142ffe708acd2
topzyh <zyh.95@163.com>
2021-09-22 16:39:10 +0800

    更新城市库

--- auto_get.php
+++ auto_get.php
@@ -13,9 +13,9 @@ if (empty($source_data['result'])) {
 
 // 数据格式化
 $source_data = explode("\n", $source_data['result']);
-$data        = [];
+$data = [];
 foreach ($source_data as $value) {
-	$value                 = explode(',', $value);
+	$value = explode(',', $value);
 	$data[(int) $value[0]] = $value;
 }
 unset($source_data);
@@ -40,19 +40,35 @@ if ($conn->query($sql) != true) {
 // 写入数据库
 echo "本次新增:<br>\n";
 foreach ($data as $value) {
-	$result = $conn->query("SELECT * FROM `system_idtoad` WHERE id=$value[0]");
+	$result = $conn->query("SELECT * FROM `idtoad` WHERE id=$value[0]");
 	if ($temp = $result->fetch_assoc()) {
-		$sql = "UPDATE `system_idtoad` SET count_name='$value[1]',city_name='$value[2]',province_name='$value[3]',status=1 WHERE id=$value[0]";
+		$sql = "UPDATE `idtoad` SET county_name='$value[1]',city_name='$value[2]',province_name='$value[3]',status=1 WHERE id=$value[0]";
 		if ($conn->query($sql) != true) {
 			echo "Error insert data: " . $conn->error . 'SQL:' . $sql . "<br>\n";
 		}
 	} else {
 		print_r($value);
 		echo "<br>\n"; // 输出新增
-		$sql = "INSERT INTO system_idtoad (id, province_name, city_name, count_name, status) VALUES ($value[0], '$value[3]', '$value[2]', '$value[1]', 1)";
+		$sql = "INSERT INTO idtoad (id, province_name, city_name, county_name, status) VALUES ($value[0], '$value[3]', '$value[2]', '$value[1]', 1)";
 		if ($conn->query($sql) != true) {
 			echo "Error insert data: " . $conn->error . 'SQL:' . $sql . "<br>\n";
 		}
 	}
 }
-echo "更新完成！";
+echo "更新完成！ <br>\n";
+
+// 更新 省市区id
+echo "更新 省市区id <br>\n";
+$result = $conn->query("SELECT * FROM `idtoad` WHERE province_id=0");
+
+while ($row = $result->fetch_assoc()) {
+	var_dump($row);
+	$_province_id = substr($row['id'], 0, 2);
+	$_city_id = substr($row['id'], 2, 2);
+	$_county_id = substr($row['id'], 4, 2);
+	$sql = "UPDATE `idtoad` SET county_id=" . $_county_id . ",city_id=" . $_city_id . ",province_id=" . $_province_id . " WHERE id=" . $row['id'];
+	if ($conn->query($sql) != true) {
+		echo "Error insert data: " . $conn->error . 'SQL:' . $sql . "<br>\n";
+	}
+}
+echo "更新 省市区id 完成<br>\n";
